f7945152ca31752ae02a7c6f17fd18dedfdfc1fc
alvingavi0 <wanyamacalvinotieno254@gmail.com>
2025-04-17 11:41:21 +0300

    Completed digital pet assignment

---
+++ main.py
@@ -0,0 +1,16 @@
+from pet import Pet
+
+# Create the pet
+my_pet = Pet("Buddy")
+
+# Interact with it
+my_pet.get_status()
+my_pet.eat()
+my_pet.play()
+my_pet.sleep()
+my_pet.get_status()
+
+# Bonus part
+my_pet.train("roll over")
+my_pet.train("play dead")
+my_pet.show_tricks()
